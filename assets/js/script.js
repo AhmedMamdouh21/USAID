ab61c24317f672aa565c9d742019d3c0f84380a8
Ahmed Mamdouh <ahmed.mamdouh2130@gmail.com>
2022-04-02 16:37:46 +0200

    LATEST NEWS - GEOGRAPHIC SCOPE - PARTNERS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
-  if ($(".navbar-fixed").length) {
-    const navBarHeight = $(".navbar-fixed").innerHeight();
-    $(".padding-top").css({
-      "padding-top": navBarHeight + "px",
-    });
-    // console.log("navBarHeight", navBarHeight);
-  }
+$(window).on("load", function () {
+  // if ($(".navbar-fixed").length) {
+  const navBarHeight = $(".navbar-fixed").innerHeight();
+  $(".padding-top").css({
+    "padding-top": navBarHeight + "px",
+  });
+  // console.log("navBarHeight", navBarHeight);
+  // }
 
   if ($(".hero").length) {
     var swiper = new Swiper(".hero-swiper", {
@@ -37,10 +37,39 @@ $(document).ready(function () {
     $(".scroll-down").on("click", function () {
       $("html, body").animate(
         {
-          scrollTop: $("#" + $(this).data("scroll")).offset().top,
+          scrollTop:
+            $("#" + $(this).data("scroll")).offset().top - navBarHeight,
         },
         1200
       );
+    });
+  }
+
+  if ($(".media-videos").length) {
+    function getId(url) {
+      const regExp =
+        /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+      const match = url.match(regExp);
+
+      return match && match[2].length === 11 ? match[2] : null;
+    }
+    $(".media-videos").on("click", function () {
+      var videosrc = $(this).attr("videosrc");
+      var empededLink = getId(videosrc);
+      $(".videoModal").modal("show");
+
+      $(".videoModal iframe").attr(
+        "src",
+        "https://www.youtube.com/embed/" +
+          empededLink +
+          "?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=1&autoplay=1"
+      );
+      console.log("videosrc", videosrc);
+      console.log("empededLink", empededLink);
+    });
+
+    $(".videoModal").on("hidden.bs.modal", function (e) {
+      $(".videoModal iframe").attr("src", "");
     });
   }
 });
